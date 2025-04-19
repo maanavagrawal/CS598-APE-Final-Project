64ad9762f4607239630306b592f96749465dc8b6
madhavp2 <madhavp2@sp25-cs598a-13.cs.illinois.edu>
2025-04-19 18:11:08 -0500

    clearing db

--- example_docker_postgres.py
+++ example_docker_postgres.py
@@ -16,18 +16,24 @@ config = {
 }
 
 if __name__ == '__main__':
+    try:
+        # create a Dejavu instance
+        djv = Dejavu(config)
 
-    # create a Dejavu instance
-    djv = Dejavu(config)
+        # Fingerprint all the mp3's in the directory we give it
+        djv.fingerprint_directory("test", [".wav"])
 
-    # Fingerprint all the mp3's in the directory we give it
-    djv.fingerprint_directory("test", [".wav"])
+        # Recognize audio from a file
+        results = djv.recognize(FileRecognizer, "mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
+        print(f"From file we recognized: {results}\n")
 
-    # Recognize audio from a file
-    results = djv.recognize(FileRecognizer, "mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
-    print(f"From file we recognized: {results}\n")
+        # Or use a recognizer without the shortcut, in anyway you would like
+        recognizer = FileRecognizer(djv)
+        results = recognizer.recognize_file("mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
+        print(f"No shortcut, we recognized: {results}\n")
 
-    # Or use a recognizer without the shortcut, in anyway you would like
-    recognizer = FileRecognizer(djv)
-    results = recognizer.recognize_file("mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
-    print(f"No shortcut, we recognized: {results}\n")
+    finally:
+        # Clean up the database
+        print("Cleaning up database...")
+        djv.db.empty()
+        print("Database cleanup complete.")
